a638e49d07337b81ab34f8334195b808f4ad6133
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-09-11 11:56:01 +0400

    autos

--- src/pages/api/data/data.ts
+++ src/pages/api/data/data.ts
@@ -1,20 +1,91 @@
-export const auto = {
-    image: "https://example.com/images/car.jpg", // URL изображения
-    brand: "Toyota", // Бренд
-    model: "Camry", // Модель
-    color: "Черный", // Цвет
-    price: 25000, // Цена
-    year: 2020, // Год выпуска
-    engineType: "Бензиновый", // Тип двигателя (Бензиновый, Дизельный, Электрический)
-    transmission: "Автоматическая", // Трансмиссия (Автоматическая, Ручная, Роботизированная)
-    range: 500 // Запас хода (в км)
-};
-
-// Проверка доступности полей в зависимости от типа двигателя
-if (auto.engineType === "Электрический") {
-    auto.transmission = undefined; // Удаляем поле transmission для электрических автомобилей
-} else {
-    auto.range = undefined; // Удаляем поле range для бензиновых и дизельных автомобилей
+enum Engine {
+    petrol = 'Бензиновый',
+    diesel = 'Дизельный',
+    electric = 'Электрический'
+}
+enum Transmission {
+    automatic = 'Автоматическая',
+    manual = 'Ручная',
+    robot = 'Роботизированная'
+}
+export type AutoTypes = {
+    image: string
+    brand: string
+    model: string
+    color: string
+    price: number
+    year: number
+    engineType: Engine
+    transmission: Transmission
+    range: number
 }
 
-console.log(auto);
+export const autos = [
+    {
+        image: "https://example.com/images/car.jpg", // URL изображения
+        brand: "Toyota", // Бренд
+        model: "Camry", // Модель
+        color: "Черный", // Цвет
+        price: 25000, // Цена
+        year: 2020, // Год выпуска
+        engineType: Engine.diesel, // Тип двигателя (Бензиновый, Дизельный, Электрический)
+        transmission: Transmission.manual, // Трансмиссия (Автоматическая, Ручная, Роботизированная)
+        range: 500 // Запас хода (в км)
+    },
+    {
+        image: "https://example.com/images/car.jpg", // URL изображения
+        brand: "BMW", // Бренд
+        model: "Camry", // Модель
+        color: "Черный", // Цвет
+        price: 25000, // Цена
+        year: 2020, // Год выпуска
+        engineType: Engine.diesel, // Тип двигателя (Бензиновый, Дизельный, Электрический)
+        transmission: Transmission.manual, // Трансмиссия (Автоматическая, Ручная, Роботизированная)
+        range: 500 // Запас хода (в км)
+    },
+    {
+        image: "https://example.com/images/car.jpg", // URL изображения
+        brand: "Ford", // Бренд
+        model: "Camry", // Модель
+        color: "Черный", // Цвет
+        price: 25000, // Цена
+        year: 2020, // Год выпуска
+        engineType: Engine.diesel, // Тип двигателя (Бензиновый, Дизельный, Электрический)
+        transmission: Transmission.manual, // Трансмиссия (Автоматическая, Ручная, Роботизированная)
+        range: 500 // Запас хода (в км)
+    },
+    {
+        image: "https://example.com/images/car.jpg", // URL изображения
+        brand: "Lada", // Бренд
+        model: "Camry", // Модель
+        color: "Черный", // Цвет
+        price: 25000, // Цена
+        year: 2020, // Год выпуска
+        engineType: Engine.diesel, // Тип двигателя (Бензиновый, Дизельный, Электрический)
+        transmission: Transmission.manual, // Трансмиссия (Автоматическая, Ручная, Роботизированная)
+        range: 500 // Запас хода (в км)
+    },
+]
+
+
+
+// export const auto: AutoTypes = {
+//     image: "https://example.com/images/car.jpg", // URL изображения
+//     brand: "Toyota", // Бренд
+//     model: "Camry", // Модель
+//     color: "Черный", // Цвет
+//     price: 25000, // Цена
+//     year: 2020, // Год выпуска
+//     engineType: EngineTypes.diesel, // Тип двигателя (Бензиновый, Дизельный, Электрический)
+//     transmission: "Автоматическая", // Трансмиссия (Автоматическая, Ручная, Роботизированная)
+//     range: 500 // Запас хода (в км)
+// };
+
+// Проверка доступности полей в зависимости от типа двигателя
+// if (auto.engineType === "Электрический") {
+//     auto.transmission = undefined; // Удаляем поле transmission для электрических автомобилей
+// } else {
+//     auto.range = undefined; // Удаляем поле range для бензиновых и дизельных автомобилей
+// }
+
+// console.log(auto);
